55ec0c04890ad6c0fc6f2bcbd04bff03abdbe64c
Salvador_Patty_Camacho <salvador.patty@ucb.edu.bo>
2025-06-02 19:52:46 -0400

    Implementacion del incognito

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,12 +2,13 @@ require 'capybara/cucumber'
 require 'selenium-webdriver'
 
 Capybara.register_driver :selenium_chrome do |app|
-	chrome_options = Selenium::WebDriver::Chrome::Options.new
+  chrome_options = Selenium::WebDriver::Chrome::Options.new
 
-	chrome_options.add_preference('profile.password_manager_enabled', false)
-	chrome_options.add_preference('credentials_enable_service', false)
-  
-	Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
+  chrome_options.add_argument('--incognito')
+  chrome_options.add_preference('profile.password_manager_enabled', false)
+  chrome_options.add_preference('credentials_enable_service', false)
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
 end
 
 Capybara.default_driver = :selenium_chrome
