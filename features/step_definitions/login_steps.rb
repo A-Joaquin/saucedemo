b6af71d1686b4e606fb5ba2350a8b9a584c29c29
RosaR20 <rosareynaga2025@gmail.com>
2025-06-17 10:24:08 -0400

    Se cambio a nombres nombres

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -3,25 +3,25 @@ Given('I am on the SauceDemo login page') do
 end
 
 When('I enter username {string}') do |username|
-  fill_in 'user-name', with: username
+  find('[data-test="username"]').set(username)
 end
 
 When('I enter password {string}') do |password|
-  fill_in 'password', with: password
+  find('[data-test="password"]').set(password)
 end
 
 When('I enter valid credentials') do
-  fill_in 'user-name', with: 'standard_user'
-  fill_in 'password', with: 'secret_sauce'
+  find('[data-test="username"]').set('standard_user')
+  find('[data-test="password"]').set('secret_sauce')
 end
 
 When('I click the login button') do
-  click_button 'Login'
+  find('[data-test="login-button"]').click
 end
 
 Then('I should be redirected to the products page') do
   expect(page).to have_current_path(/inventory\.html/, url: true)
-  expect(page).to have_content('Products')
+  expect(page).to have_css('.title', text: 'Products')
 end
 
 Then('I should see the error message {string}') do |msg|
